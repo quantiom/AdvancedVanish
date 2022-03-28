51bb778ee9a595c5065507bbc0f18345097d88f0
quantiom <quantioms@gmail.com>
2022-03-27 21:37:44 -0400

    Update ActionBarHook to support newer versions

--- src/main/kotlin/me/quantiom/advancedvanish/hook/impl/ActionBarHook.kt
+++ src/main/kotlin/me/quantiom/advancedvanish/hook/impl/ActionBarHook.kt
@@ -8,32 +8,46 @@ import me.quantiom.advancedvanish.event.PlayerVanishEvent
 import me.quantiom.advancedvanish.hook.IHook
 import me.quantiom.advancedvanish.util.AdvancedVanishAPI
 import me.quantiom.advancedvanish.util.color
+import me.quantiom.advancedvanish.util.isVanished
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.scheduler.BukkitRunnable
 
 class ActionBarHook : IHook {
     private val updateTask: BukkitRunnable =
         object : BukkitRunnable() {
             override fun run() {
-                if (!Bukkit.getPluginManager().isPluginEnabled("ActionBarAPI")) return
-
-                AdvancedVanishAPI.vanishedPlayers.map(Bukkit::getPlayer).forEach(::sendActionBar)
+                AdvancedVanishAPI.vanishedPlayers.map(Bukkit::getPlayer).map { it!! }.forEach(::sendActionBar)
             }
         }
 
     override fun getID() = "ActionBar"
 
     override fun onEnable() {
-        this.updateTask.runTaskTimer(AdvancedVanish.instance, 0L, 40L)
+        this.updateTask.runTaskTimer(AdvancedVanish.instance!!, 0L, 40L)
     }
 
     override fun onDisable() {
         this.updateTask.cancel()
     }
 
-    private fun sendActionBar(player: Player) = ActionBarAPI.sendActionBar(player, Config.getValueOrDefault("messages.action-bar", "&cYou are in vanish.").color())
+    private fun sendActionBar(player: Player) {
+        this.sendActionBarStr(player, Config.getValueOrDefault("messages.action-bar", "&cYou are in vanish."))
+    }
+
+    private fun sendActionBarStr(player: Player, str: String) {
+        try {
+            Class.forName("net.md_5.bungee.api.ChatMessageType")
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent(str.color()))
+        } catch (e: ClassNotFoundException) {
+            ActionBarAPI.sendActionBar(player, str.color())
+        }
+    }
 
     @EventHandler
     private fun onVanish(event: PlayerVanishEvent) {
@@ -42,6 +56,6 @@ class ActionBarHook : IHook {
 
     @EventHandler
     private fun onUnVanish(event: PlayerUnVanishEvent) {
-        ActionBarAPI.sendActionBar(event.player, "")
+        this.sendActionBarStr(event.player, "")
     }
 }
